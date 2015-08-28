03abe740d31ba98721e668794159b403a94f767b
acrispin <antonio.crispin@gmail.com>
2015-08-28 03:58:25 -0500

    update

--- app.py
+++ app.py
@@ -9,10 +9,29 @@ import momoko
 from settings import settings
 from urls import url_patterns
 from handlers.error import ErrorHandler
-from utils.env import dsn, http_port
+from utils.env import dsn_dev, dsn_prd, http_port
 
 if __name__ == '__main__':
     try:
+        flagport = False
+        countargv = len(sys.argv)
+
+        # $ python app.py || $ python app.py 1234 || # $ python app.py d ||  $ python app.py 1234 d
+        if countargv >= 2 and sys.argv[1].isdigit():
+            port = int(sys.argv[1])
+            flagport = True
+        else:
+            port = http_port            
+        
+        if (countargv == 2 and sys.argv[1] == 'd') \
+           or (countargv == 3 and sys.argv[2] == 'd') :
+            dsn = dsn_dev
+        else:
+            dsn = dsn_prd
+
+        # usado para deployar en heroku y evitar el error : Web process failed to bind to $PORT within 60 seconds of launch
+        port = int(os.environ.get('PORT', port))
+        
         tornado.web.ErrorHandler = ErrorHandler
         tornado.options.parse_command_line()             
 
@@ -22,18 +41,11 @@ if __name__ == '__main__':
             size = 5
         )
 
-        if len(sys.argv) == 2 and sys.argv[1].isdigit():
-            port = int(sys.argv[1])
-        else:
-            port = http_port
-
-        # usado para deployar en heroku y evitar el error : Web process failed to bind to $PORT within 60 seconds of launch
-        port = int(os.environ.get('PORT', port))
-        
         http_server = HTTPServer(application)
         http_server.listen(port, '0.0.0.0')
 
         print "Iniciando app en", port
+        print "dsn", dsn
 
         IOLoop.instance().start()
 
--- utils/env.py
+++ utils/env.py
@@ -1,22 +1,25 @@
 import os
 
 # credenciales local
+db_host = os.environ.get('MOMOKO_TEST_HOST', 'localhost')
+db_port = os.environ.get('MOMOKO_TEST_PORT', 5432)
 db_database = os.environ.get('MOMOKO_TEST_DB', 'basedb')
 db_user = os.environ.get('MOMOKO_TEST_USER', 'development')
 db_password = os.environ.get('MOMOKO_TEST_PASSWORD', '12345678')
-db_host = os.environ.get('MOMOKO_TEST_HOST', 'localhost')
-db_port = os.environ.get('MOMOKO_TEST_PORT', 5432)
 
 # enable_hstore = True if os.environ.get('MOMOKO_TEST_HSTORE', False) == '1' else False
 
-# credenciales heroku db
-# db_database = os.environ.get('MOMOKO_TEST_DB', 'd3gfgcvud4enli')
-# db_user = os.environ.get('MOMOKO_TEST_USER', 'gywbioexrnbuau')
-# db_password = os.environ.get('MOMOKO_TEST_PASSWORD', 'nKZuOnJVkZAfyU2eSNT0MvYu4k')
-# db_host = os.environ.get('MOMOKO_TEST_HOST', 'ec2-54-227-255-240.compute-1.amazonaws.com')
-# db_port = os.environ.get('MOMOKO_TEST_PORT', 5432)
+dsn_dev = 'dbname=%s user=%s password=%s host=%s port=%s' % ( \
+    db_database, db_user, db_password, db_host, db_port)
 
-dsn = 'dbname=%s user=%s password=%s host=%s port=%s' % (
+# credenciales heroku db
+db_host = os.environ.get('MOMOKO_TEST_HOST', 'ec2-54-227-255-240.compute-1.amazonaws.com')
+db_port = os.environ.get('MOMOKO_TEST_PORT', 5432)
+db_database = os.environ.get('MOMOKO_TEST_DB', 'd3gfgcvud4enli')
+db_user = os.environ.get('MOMOKO_TEST_USER', 'hxrysbuozjzdqb')
+db_password = os.environ.get('MOMOKO_TEST_PASSWORD', 'hDNih6CmgMuaoUsvaeX8KwBigy')
+
+dsn_prd = 'dbname=%s user=%s password=%s host=%s port=%s' % ( \
     db_database, db_user, db_password, db_host, db_port)
 
 http_port = 8080
